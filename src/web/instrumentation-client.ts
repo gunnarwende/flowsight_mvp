e568f759a13b70ca8e961c114739cbb3ae711698
Gunnar Wende <gunnar.wende@example.com>
2026-02-17 19:43:52 +0100

    chore: add web app + sentry mvp config (2026-02-17 19:43)

---
+++ src/web/instrumentation-client.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e86f9aaf5d34929e8b0a8279fdc5bc7d@o4510902144401408.ingest.de.sentry.io/4510902158950480",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
